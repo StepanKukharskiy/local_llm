b1d496c39ddcc1c6b486a83ceaaa3423507d0522
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-05-25 14:36:13 +0000

    remote models

--- src/routes/api/transformers/[query]/+server.js
+++ src/routes/api/transformers/[query]/+server.js
@@ -1,21 +1,21 @@
 import { pipeline, env } from '@xenova/transformers';
-import { fileURLToPath } from 'url';
-import path from 'path';
+// import { fileURLToPath } from 'url';
+// import path from 'path';
 
-// Get the directory name of the current module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// // Get the directory name of the current module
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-console.log(__filename)
-console.log(__dirname)
+// console.log(__filename)
+// console.log(__dirname)
 // console.log(`/var/task/.netlify/server/entries`)
 
-// env.allowLocalModels = false;
-// env.allowRemoteModels = true;
+env.allowLocalModels = false;
+env.allowRemoteModels = true;
 
-env.allowLocalModels = true;
-env.localModelPath = 'https://local-llms-test.netlify.app/models/';
-env.allowRemoteModels = false;
+// env.allowLocalModels = true;
+// env.localModelPath = 'https://local-llms-test.netlify.app/models/';
+// env.allowRemoteModels = false;
 
 export async function GET({ locals, params }) {
 
