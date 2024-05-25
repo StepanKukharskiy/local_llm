e55d8abefb83957c99e840dbee602ea5da14fda3
Stepan Kukharskiy <stepan.kukharskiy@gmail.com>
2024-05-25 07:59:45 +0000

    local llms test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	let query = '';
+	let messages = [],
+		queries = [];
+	async function generateAnswer() {
+		const message = await fetch(`/api/transformers/${query}`);
+		const messageObject = await message.json();
+		queries = [...queries, query];
+		messages = [...messages, messageObject.response];
+	}
+</script>
+
+<div class="container">
+	<textarea bind:value={query}></textarea>
+	<button on:click={generateAnswer}>Generate</button>
+	{#each messages as message, i}
+		<p>query</p>
+		<p class="query">{queries[i]}</p>
+		<p>answer</p>
+		<p class="response">{messages[i]}</p>
+	{/each}
+</div>
+
+<style>
+	.container {
+		display: grid;
+	}
+	.response {
+		padding: 10px;
+		border: 1px solid #1a1a1a;
+		border-radius: 10px;
+		background-color: rgb(215, 242, 253);
+		margin: 5px;
+	}
+	.query {
+		padding: 10px;
+		border: 1px solid #1a1a1a;
+		border-radius: 10px;
+		background-color: rgb(240, 240, 240);
+		margin: 5px;
+	}
+</style>
---
+++ src/routes/api/transformers/[query]/+server.js
@@ -0,0 +1,41 @@
+import { pipeline, env } from '@xenova/transformers';
+
+// env.allowLocalModels = false;
+// env.allowRemoteModels = true;
+
+env.allowLocalModels = true;
+env.localModelPath = "$lib/models";
+env.allowRemoteModels = false;
+
+export async function GET({ locals, params }) {
+
+    try {
+        const prompt = params.query;
+
+        const generator = await pipeline(
+            "text2text-generation",
+            "Xenova/LaMini-Flan-T5-248M",
+        );
+
+        const generatorOutput = await generator(
+            prompt,
+            {
+                temperature: 2,
+                max_new_tokens: 150,
+                repetition_penalty: 1.5,
+                no_repeat_ngram_size: 3,
+                num_beams: 2,
+                num_return_sequences: 1,
+            },
+        );
+
+        const rephraseObject = {
+            response: generatorOutput[0].generated_text
+        }
+
+        return new Response(JSON.stringify(rephraseObject))
+
+    } catch (err) {
+        console.log(err)
+    }
+}
